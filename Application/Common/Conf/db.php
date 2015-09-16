b65f74e423d6d06884ee60d4a667b9abcf77e88a
jksbill <313032758@qq.com>
2015-09-16 09:20:42 +0800

    修改数据库配置

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -1,12 +1,18 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    'URL_MODEL' => 'REWRITE', 
+    'DB_TYPE'               =>  'mysqli',     // 数据库类型
+    'DB_HOST'               =>  '114.215.177.177', // 服务器地址
+    'DB_NAME'               =>  'accounting',          // 数据库名
+    'DB_USER'               =>  'wsd',      // 用户名
+    'DB_PWD'                =>  'wsd',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    /*'URL_MODEL' => 'REWRITE', 
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'accounting', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => '123456', // 密码
     'DB_PORT'   => 3306, // 端口
-    'DB_PREFIX' => '', // 数据库表前缀
+    'DB_PREFIX' => '', // 数据库表前缀*/
 );
